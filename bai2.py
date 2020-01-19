b32bd99ae03f7ef83a482bd169cc51ec874c5500
Kien <kienga445@gmail.com>
2020-01-19 12:02:15 +0700

    Add files via upload

---
+++ bai2.py
@@ -0,0 +1,17 @@
+def sum1(arr):
+    return sum(arr)
+
+arr = []
+arr = [25, 523, 225, 2436, 24, 6453, 24]
+ans = sum1(arr)
+print(sum(arr))
+
+maxnumber = max(arr)
+print (maxnumber)
+
+minnumber = min(arr)
+print (minnumber)
+
+x = int(input("Number: "))
+print(arr.count(x))
+
---
+++ uocso.py
@@ -0,0 +1,10 @@
+a = int(input("Your number = "))
+b = int(input("Your second number: "))
+import math
+print(math.gcd(a,b))
+
+
+
+
+from fractions import Fraction
+print (Fraction(a,b))
